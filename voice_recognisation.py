c5a573cc8238fdaaf8523af8ff7644692d79e33e
Gaurav Raghav <78542102+gauravraghav1988@users.noreply.github.com>
2021-02-18 14:58:22 +0530

    Update voice_recognisation.py

--- voice_recognisation.py
+++ voice_recognisation.py
@@ -5,6 +5,13 @@ with s.Microphone() as m:
     audio= sr.listen(m)
     query=sr.recognize_google(audio,language="en-us")
     print(query)
+    
+    
+import pyttsx3                         #this code here will speak out your said voice command
+engine = pyttsx3.init()
+engine.say(query)
+engine.runAndWait()
+
 
   
   
